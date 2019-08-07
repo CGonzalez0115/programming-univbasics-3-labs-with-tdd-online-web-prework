b027115eff4f71ba39e93ef0a6f280757fd44b08
Christina Gonzalez <cmgamway@gmail.com>
2019-08-07 04:23:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 
-when "contains a local variable called first_number that is assigned to a number" do 
-  first_number = 9.0 
+when "contains a local variable called first_number that is assigned to a number" do
+  first_number = 9.0
 end
